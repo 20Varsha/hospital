cc0b58eb5ff143d68c6b940719566bcf1f820359
varsha p m <varshapm313@gmail.com>
2022-03-05 13:57:49 +0530

    hospital

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Clinic from './Clinic'
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Clinic/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clinic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ function Clinic() {
+ 
+ 
+    return <div>
+<h1>Welcome</h1>
+    </div>
+}
+export default Clinic
